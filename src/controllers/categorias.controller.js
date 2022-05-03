dcc3bf1fb75c4a34cb7c2d8d2a8fb3a86b0ed203
MateoHincapieA <mateohincapiearias@gmail.com>
2022-05-03 13:57:18 -0500

    Separación de la información de la BD y then catch

--- src/controllers/categorias.controller.js
+++ src/controllers/categorias.controller.js
@@ -9,42 +9,70 @@ const pool = new Pool ({
 })
 
 const getCategorias = async(req, res) => {
-    const response = await pool.query('SELECT * FROM categorias');
-    res.status(200).json(response.rows);
+    await pool.query('SELECT * FROM categorias')
+        .then(response => {
+            res.status(200).json(response.rows);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    
 }
 
 const getCategoriasById = async(req, res) => {
     const { id } = req.body;
-    const response = await pool.query('SELECT * FROM categorias WHERE id = $1', [id]);
-    res.status(200).json(response.rows);
+    await pool.query('SELECT * FROM categorias WHERE id = $1', [id])
+        .then(response => {
+            res.status(200).json(response.rows);
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
 }
 
 const createCategoria = async(req, res) => {
     const { nombre, descripcion } = req.body;
-    const response = await pool.query('INSERT INTO categorias(nombre, descripcion) VALUES($1, $2)', [nombre, descripcion]);
-    res.json({
-        message: 'Categoría agregada satisfactoriamente',
-        body: {
-            categoria: {nombre, descripcion}
-        }
-    });
+    await pool.query('INSERT INTO categorias(nombre, descripcion) VALUES($1, $2)', [nombre, descripcion])
+        .then(response => {
+            res.json({
+                message: 'Categoría agregada satisfactoriamente',
+                body: {
+                    categoria: {nombre, descripcion}
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
 }
 
 const deleteCategoria = async(req, res) => {
     const { id } = req.body;
-    const response = await pool.query('DELETE FROM categorias WHERE id = $1', [id]);
-    res.json(`Categoria ${id} eliminada satisfactoriamente`);
+    await pool.query('DELETE FROM categorias WHERE id = $1', [id])
+        .then(response => {
+            res.json(`Categoria ${id} eliminada satisfactoriamente`);
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
+    
 }
 
 const updateCategoria = async(req, res) => {
     const { id, nombre, descripcion } = req.body;
-    const response = await pool.query('UPDATE categorias SET nombre= $1, descripcion = $2 WHERE id = $3', [nombre, descripcion, id]);
-    res.json({
-        message: 'Categoría actualizada satisfactoriamente',
-        body: {
-            categoria: {nombre, descripcion}
-        }
-    });
+   await pool.query('UPDATE categorias SET nombre= $1, descripcion = $2 WHERE id = $3', [nombre, descripcion, id])
+    .then(response => {
+        res.json({
+            message: 'Categoría actualizada satisfactoriamente',
+            body: {
+                categoria: {nombre, descripcion}
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    }); 
+    
 }
 
 module.exports = {
